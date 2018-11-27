c4a28449163462573012ebd2d19804c843ee357e
Libby Hughes <libbyhughes@Libbys-MacBook-Pro.local>
2018-11-26 20:33:15 -0700

    bulk of files

---
+++ app/data/friends.js
@@ -0,0 +1,106 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends
+// ===============================================================================
+
+var friends = [
+    {
+      name:"Aragorn",
+      photo:"https://en.wikipedia.org/wiki/Aragorn#/media/File:Aragorn300ppx.png",
+      scores:[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Frodo Baggins",
+      photo: "https://en.wikipedia.org/wiki/Frodo_Baggins#/media/File:Elijah_Wood_as_Frodo_Baggins.png",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Gollum",
+      photo: "https://en.wikipedia.org/wiki/Gollum#/media/File:Gollum.PNG",
+      scores: [
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "5"
+      ]
+    },
+    {
+      name: "Radagast",
+      photo: "https://en.wikipedia.org/wiki/Radagast#/media/File:Radagast_the_Brown_Hobbit_promotional_photo.jpg",
+      scores: [
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "3",
+        "2",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Legolas",
+      photo: "https://en.wikipedia.org/wiki/Legolas#/media/File:Legolas600ppx.jpg",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+    },
+    {
+      name: "Sam",
+      photo: "https://en.wikipedia.org/wiki/Samwise_Gamgee#/media/File:Sean_Astin_as_Samwise_Gamgee.png",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "2",
+        "1",
+        "5",
+        "4",
+        "3",
+        "2",
+        "1"
+      ]
+    }
+  ]
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
